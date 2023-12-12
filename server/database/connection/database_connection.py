6850a05e13ed93bc5aa9d83af0f80384ea071ded
Norbert Remenyi <remenyinorbert@gmail.com>
2023-12-12 10:37:30 +0100

    Chore: add try and expect to initialize db method

--- server/database/connection/database_connection.py
+++ server/database/connection/database_connection.py
@@ -1,21 +1,24 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, pool
 from sqlalchemy.orm import sessionmaker
 from server.database.models.secret_model import Base
 import os
 
-db_username = os.environ.get("DB_USERNAME")
-db_password = os.environ.get("DB_PASSWORD")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 
 def initialize_db(created_engine):
-    db_name = "iss"
-    with created_engine.connect() as connection:
-        connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {db_name}"))
-        connection.execute(text(f"USE {db_name}"))
+    try:
+        db_name = "iss"
+        with created_engine.connect() as connection:
+            connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {db_name}"))
+            connection.execute(text(f"USE {db_name}"))
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 
-connection_url = f'mysql+mysqlconnector://{db_username}:{db_password}@localhost:3306/'
-engine = create_engine(connection_url, echo=True)
+connection_url = f'mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@localhost:3306/'
+engine = create_engine(connection_url, poolclass=pool.QueuePool, echo=True)
 initialize_db(engine)
 Base.metadata.create_all(engine)
 
